18bcb5c1fc6626b465e8695381c116b8c4c56462
Mingchen Ju <traveller31415@gmail.com>
2024-12-28 04:25:22 +0000

    重构 SqlAccess 类以支持异步数据库初始化

--- src/sqlAccess.ts
+++ src/sqlAccess.ts
@@ -1,13 +1,22 @@
 import { Database } from "sqlite3";
 
 export default class SqlAccess {
-  private db: Database;
+  private db!: Database;
 
   constructor(dbRoute: string) {
-    this.db = new Database(dbRoute, (err) => {
-      if (err) {
-        console.error(err.message);
-      }
+    this.init(dbRoute);
+  }
+
+  private async init(dbRoute: string) {
+    this.db = await new Promise<Database>((resolve, reject) => {
+      const db = new Database(dbRoute, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log("Database connected successfully.");
+          resolve(db);
+        }
+      });
     });
   }
 
--- test/sqlAccess.test.ts
+++ test/sqlAccess.test.ts
@@ -2,7 +2,4 @@ import sqlAccess from "../src/sqlAccess";
 
 test("SqlAccess", () => {
     const db = new sqlAccess("/workspace/test123.db");
-    // const db = new sqlAccess("/workspace/test.db");
-
-    db.printDb();
 });
