9b0bbec686de086b2fef50dfe12a49e955267aca
Mingchen Ju <traveller31415@gmail.com>
2024-12-29 09:15:49 +0000

    update on test

--- test/sqlAccess.test.ts
+++ test/sqlAccess.test.ts
@@ -1,7 +1,12 @@
 import sqlAccess from '../src/sqlAccess';
+import fs from 'fs';
 
 test('SqlAccess', async () => {
-  const db = new sqlAccess('/workspaces/AusPropertySales/test/test.db');
+  const testDir = '/workspaces/AusPropertySales/test/testFiles';
+  if (!fs.existsSync(testDir)) {
+    fs.mkdirSync(testDir, { recursive: true });
+  }
+  const db = new sqlAccess('/workspaces/AusPropertySales/test/testFiles/test.db');
 
   await db.hold;
   await db.setupSchema();
